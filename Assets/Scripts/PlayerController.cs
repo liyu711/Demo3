5499ab5bf300d0c32001f89fbfbe30c7ccd74046
Yudong Li <a1923172548@gmail.com>
2025-03-27 22:56:44 -0700

    Added jump and death animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerBody;
+    private Animator playerAnim;
     private float impulse = 10;
     // prevent double jump
     public bool isOnGround = true;
     public bool gameOver = false;
 
+
     // Start is called before the first frame update
     void Start()
     {
         playerBody = GetComponent<Rigidbody>();
-                
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerBody.AddForce(Vector3.up * impulse, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
+
         
     }
 
@@ -35,10 +39,13 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
         }
+        // If collide with obstacle the character dies
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("game over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
         
     }
